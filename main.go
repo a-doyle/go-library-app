36592d50e4d3a72019f62c6a2868f024b6ca2c1f
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-20 11:40:04 +0000

    updated to include a template

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Go Web Development")
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
