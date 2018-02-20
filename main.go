403446149bc0fec91a92533d1571c3f0c0eedc22
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-20 11:51:58 +0000

    updated main to allow for dynamic querying

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"html/template"
 )
 
+type Page struct {
+	Name string
+}
+
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
+	// dynamically sets the name from the query parameter
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+		p := Page{Name: "Abaddon"}
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+
+		// returns an error object if present
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
